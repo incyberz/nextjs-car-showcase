922aec1673859d86ea393a3a7b209fbba8a14733
incyberz <incyberz@yahoo.com>
2023-09-01 08:45:14 +0700

    form search UI fixed

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -8,16 +8,38 @@ import SearchButton from "./SearchButton";
 const SearchBar = () => {
   const handleSearch = () => {};
   const [manufacturer, setManufacturer] = useState("");
+  const [model, setModel] = useState("");
 
   return (
-    <form onSubmit={handleSearch}>
+    <form onSubmit={handleSearch} className="searchbar debug">
       <div className="searchbar__item">
         <SearchManufacturer
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
         />
-        <SearchButton otherClasses="zzzsm:hidden" />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
+
+      <div className="searchbar__item">
+        <Image
+          src={"/model-icon.png"}
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px] ml-4"
+          alt="car model icon"
+        />
+
+        <input
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Tiguan"
+          className="searchbar__input debug-blue"
+        />
+        <SearchButton otherClasses="sm:hidden debug-yellow" />
+      </div>
+      <SearchButton otherClasses="max-sm:hidden debug" />
     </form>
   );
 };
